5ce9d0435d6186647c614e8c2fa075ea0a17a049
fai-sal-786 <154748475+fai-sal-786@users.noreply.github.com>
2024-05-03 05:15:29 -0700

    AGE CALCULATE & ATTENDANCE

---
+++ date/app12.js
@@ -0,0 +1,46 @@
+// var now = new Date();
+// var bd = new Date (prompt("ENTER DATE"));
+// var mileseconds = now.getTime();
+// var bdmileseconds = bd.getTime();
+// var diff = mileseconds - bdmileseconds;
+// console.log("mileseconds=>", diff);
+// console.log("seconds=>", Math.round(diff / 1000));
+// console.log("minutes=>", Math.round(diff / 1000 / 60));
+// console.log("hours=>", Math.round(diff / 1000 / 60 / 60));
+// console.log("day=>", Math.round(diff / 1000 / 60 / 60 / 24));
+// console.log("months=>", Math.round(diff / 1000/ 60 / 60 / 24/ 30));
+// console.log("year=>", Math.round(diff / 1000 / 60 / 60 / 24/ 30/ 12));
+
+// var now = new Date();
+// var bd = new Date ("9/25/1997");
+// var mileseconds = now.getTime();
+// var bdmileseconds = bd.getTime();
+// var diff = mileseconds - bdmileseconds;
+// console.log("mileseconds=>", diff);
+// console.log("seconds=>", Math.round(diff / 1000));
+// console.log("minutes=>", Math.round(diff / 1000 / 60));
+// console.log("hours=>", Math.round(diff / 1000 / 60 / 60));
+// console.log("day=>", Math.round(diff / 1000 / 60 / 60 / 24));
+// console.log("months=>", Math.round(diff / 1000/ 60 / 60 / 24/ 30));
+// console.log("year=>", Math.round(diff / 1000 / 60 / 60 / 24/ 30/ 12));
+
+
+
+
+// var attendanceTime = new Date();
+// attendanceTime.setHours(17);
+// attendanceTime.setMinutes(60);
+// attendanceTime.setTime(0);
+// console.log(attendanceTime);
+// var now = new Date();
+// var diff = now.getTime() - attendanceTime.getTime();
+// var minutes = Math.floor(diff / 1000 / 60);
+// console.log("minutes=>", minutes);
+// if(minutes <= 0) {
+    // console.log("you are on time. keep it up!");
+// } else if (minutes <= 5){
+    // console.log("Bach gae aaj. kal se fix 45 se pehle aajana");
+// } else {
+//  
+    // console.log("you are late salary kat gaye yeh!");
+// }
